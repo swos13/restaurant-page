cf8a0454876304b6bd6dbac07fd3b1a3a5becabc
swos <szymon.wos1319@gmail.com>
2024-02-03 13:25:22 +0100

    Style header

--- src/home.js
+++ src/home.js
@@ -1,7 +1,6 @@
 const displayController = (()=> {
 
     const addElementsToContainer = (container) => {
-        const mainHeader = document.createElement('h1');
         const brandSlogan = document.createElement('h3');
         const mainText = document.createElement('p');
         const menu = document.createElement('p');
@@ -9,7 +8,6 @@ const displayController = (()=> {
         const phone = document.createElement('p');
         const open = document.createElement('p');
 
-        mainHeader.textContent = "Cocinera";
         brandSlogan.textContent = "Unique Italian taste";
         mainText.textContent = "We invite you to try our most delicious italian meals in our restaurant in heart of Poznan";
         menu.textContent = "Check out our ";
@@ -20,7 +18,6 @@ const displayController = (()=> {
         menuLink.href = "./menu.html";
         menu.appendChild(menuLink);
 
-        container.appendChild(mainHeader);
         container.appendChild(brandSlogan);
         container.appendChild(mainText);
         container.appendChild(menu);
