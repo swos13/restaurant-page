d523df9726407fd775f2e0f25cf416d3955a33d7
swos <szymon.wos1319@gmail.com>
2024-02-05 03:05:45 +0100

    Add html webpack plugin and clear files each build

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import menuDisplayController from './menu.js';
 import aboutDisplayController from './about.js';
 import contactDisplayController from './contact.js';
 
+function clearContainer(container) {
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+}
+
 const homeButton = document.querySelector('.home-button');
 const menuButton = document.querySelector('.menu-button');
 const aboutButton = document.querySelector('.about-button');
@@ -11,3 +17,23 @@ const contactButton = document.querySelector('.contact-button');
 
 const content = document.querySelector('#content');
 homeDisplayController.displayContent(content);
+
+homeButton.addEventListener('click', () => {
+    clearContainer(content);
+    homeDisplayController.displayContent(content);
+});
+
+menuButton.addEventListener('click', () => {
+    clearContainer(content);
+    menuDisplayController.displayContent(content);
+});
+
+aboutButton.addEventListener('click', () => {
+    clearContainer(content);
+    aboutDisplayController.displayContent(content);
+});
+
+contactButton.addEventListener('click', () => {
+    clearContainer(content);
+    contactDisplayController.displayContent(content);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
     devtool: 'source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
