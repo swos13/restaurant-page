9d8577763895b67b4f7c0fd35e29970cb534ec66
swos <szymon.wos1319@gmail.com>
2024-02-06 17:00:54 +0100

    Add text files and txt loader

--- src/about.js
+++ src/about.js
@@ -1,7 +1,18 @@
+import OurFood from './data/about/our-food.txt';
+import OurHistory from './data/about/our-history.txt';
+
 const displayController = (()=> {
 
     const displayContent = (container) => {
-        
+        container.setAttribute('class', 'about');
+        const header = document.createElement('h2');
+        const foodHeader = document.createElement('h4');
+        const foodPara = document.createElement('p');
+        const historyHeader = document.createElement('h4');
+        const historyPara = document.createElement('p');
+
+        console.log(OurFood);
+        console.log(OurHistory);
     }
 
     return { displayContent };
